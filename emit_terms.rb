01b1886a5254a1981ca5b01aa8abe81fa1043734
mat <matthew.kelcey@gmail.com>
2009-09-10 21:15:01 +1000

    diy combiner in calc token freq step

--- emit_terms.rb
+++ emit_terms.rb
@@ -1,6 +1,22 @@
 #!/usr/bin/env ruby
+# diy combiner by emiting only when there are a 1000 unique terms encountered
+
+class Hash
+	def inc key
+		self[key] ||= 0
+		self[key] += 1
+		dump if keys.size > 1000
+	end
+	def dump
+		each { |k,v| puts "LongValueSum:#{k}.0p\t#{v}" }
+		clear
+	end
+end
+
+freq = {}
 STDIN.each do |line|
 	line_without_filename = line.chomp.sub /(.*?) /,''
-	terms = line_without_filename.split
-	terms.each { |term| puts "LongValueSum:#{term}.0p\t1" }
+	tokens = line_without_filename.split
+	tokens.each { |token| freq.inc token }
 end
+freq.dump
